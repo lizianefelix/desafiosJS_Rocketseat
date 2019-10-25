f3908284a7ecb7d41de9a2babe3c19c38ba569f1
L. Felix dos Anjos <l.felix.dos.anjos@avanade.com>
2019-10-25 17:37:47 -0300

    chore: organização do código javascript

--- challengeTwo/challenge.js
+++ challengeTwo/challenge.js
@@ -1,14 +1,17 @@
 const btnChallenge = document.getElementById('btn_desafiar');
 const valueSelect = document.getElementById('numero_desafio');
+const divQuadrado = document.getElementById('div_quadrado');
 
 btnChallenge.onclick = function () {
+    var divBtn = document.getElementById('div_btn');
+
+    divBtn.innerHTML = "";
+    divQuadrado.innerHTML = "";
 
     if (valueSelect.value == 1 || valueSelect.value == 2) {
 
-        const divBtn = document.getElementById('div_btn');
-        const divQuadrado = document.getElementById('div_quadrado');
-        const btnCreate = document.createElement('input');
-        const btnDelete = document.createElement('input');
+        var btnCreate = document.createElement('input');
+        var btnDelete = document.createElement('input');
 
         btnCreate.setAttribute('type', 'button');
         btnCreate.setAttribute('value', 'Gerar Quadrado');
@@ -19,6 +22,7 @@ btnChallenge.onclick = function () {
         btnCreate.style.padding = '10px';
         btnCreate.style.marginRight = '5px';
         btnCreate.style.border = '0px';
+        btnCreate.onclick = gerarQuadrado;
 
         btnDelete.setAttribute('type', 'button');
         btnDelete.setAttribute('value', 'Deletar Quadrado');
@@ -28,57 +32,11 @@ btnChallenge.onclick = function () {
         btnDelete.style.color = '#fff';
         btnDelete.style.padding = '10px';
         btnDelete.style.border = '0px';
+        btnDelete.onclick = deletarQuadrado;
 
         divBtn.appendChild(btnCreate);
         divBtn.appendChild(btnDelete);
 
-        const btnGerarQuadrado = document.getElementById('gerar_quadrado');
-        const btnDeletarQuadrado = document.getElementById('deletar_quadrado');
-
-        let quadrado;
-        btnGerarQuadrado.onclick = function () {
-            quadrado = document.createElement('div');
-
-            quadrado.style.width = '100px';
-            quadrado.style.height = '100px';
-            quadrado.style.backgroundColor = '#E63462';
-            quadrado.style.marginTop = '10px';
-
-            divQuadrado.appendChild(quadrado);
-
-            quadrado.onmouseover = function () {
-                var newColor = getRandomColor();
-                quadrado.style.backgroundColor = newColor;
-            }
-
-            // const legnthQuadrados = document.querySelectorAll('#div_quadrado div');
-            // if (legnthQuadrados.length > 0) {
-            //     for (quadrados of legnthQuadrados) {
-            //         quadrados.onmouseover = function () {
-            //             var newColor = getRandomColor();
-            //             quadrado.style.backgroundColor = newColor;
-            //         }
-            //     }
-            //     // if (valueSelect.value == 2) {
-            //     //     quadrado.onmouseover = function () {
-            //     //         quadrado.style.backgroundColor = newColor;
-            //     //     }
-            //     // }
-            // }
-        }
-
-        btnDeletarQuadrado.onclick = function () {
-            divQuadrado.firstChild.remove();
-        }
-
-        function getRandomColor() {
-            var letters = "0123456789ABCDEF";
-            var color = "#";
-            for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
     }
     else if (valueSelect.value == 3) {
 
@@ -93,3 +51,39 @@ btnChallenge.onclick = function () {
 
     }
 };
+
+//desafio 01+02
+function gerarQuadrado() {
+    var quadrado = document.createElement('div');
+
+    quadrado.style.width = '100px';
+    quadrado.style.height = '100px';
+    quadrado.style.backgroundColor = '#E63462';
+    quadrado.style.marginTop = '10px';
+
+    divQuadrado.appendChild(quadrado);
+
+    if (valueSelect.value == 2) {
+        quadrado.onmouseover = mudarCor;
+    }
+}
+
+//desafio 01+02
+function deletarQuadrado() {
+    divQuadrado.firstChild.remove();
+}
+
+//desafio 02
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+//desafio 02
+function mudarCor() {
+    this.style.backgroundColor = getRandomColor();
+}
